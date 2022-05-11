e0fbb9306cdcb7ca721248386a8e5b9ed7fcc26e
Roman Kovtunenko <roman.kovtunenko@gmail.com>
2022-05-11 17:02:18 +0300

    Remove configuration of generators

--- config/application.rb
+++ config/application.rb
@@ -25,10 +25,5 @@ module TardisUser
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.generators do |g|
-      g.test_framework :rspec
-      g.stylesheets false
-    end
   end
 end
