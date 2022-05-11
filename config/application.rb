0c8556caa4217058054dda74284c4c6467796790
Roman Kovtunenko <roman.kovtunenko@gmail.com>
2022-05-11 16:53:03 +0300

    Add rspec

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,10 @@ module TardisUser
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.stylesheets false
+    end
   end
 end
